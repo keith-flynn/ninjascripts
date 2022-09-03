2a6f60e6c0990bba98358ca41970e2b785e67592
wingnutboogeyman <84752530+wingnutboogeyman@users.noreply.github.com>
2022-09-03 18:07:51 -0400

    list unique machine types and their total count
    NEEDS to run as admin

---
+++ machine_types.py
@@ -0,0 +1,26 @@
+#run as admin
+
+#import text: File NEEDS to be named "input.txt" and saved to the C:\ directory
+with open(r"C:\input.txt", 'r') as f:
+    first = f.read()
+
+#split data into a list of separate strings
+second = first.split()
+
+
+#create new list of ONLY unique strings...
+#this saves each serial, but also keeps a few stragglers such as PO, EID, timestamps, and machine types
+third = []
+for i in second:
+    if i not in third:
+        third.append(i)
+
+#keeps only list items with 'REC-'
+to_keep = ['REC-']
+fourth = [x for x in third if any(y in x for y in to_keep)]
+
+#for as many uniqe models that exist in the list,
+#print which model they are and how many times each occurs (total number)
+for models in fourth:
+    print(f'{models} {second.count(models)}')
+
