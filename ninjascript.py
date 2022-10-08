0218f8910fbebfdfc25735b8bfcfba7951dd57ae
Keith Flynn <flynn.keith@gmail.com>
2022-10-07 22:19:54 -0400

    added gui file select

--- ninjascript.py
+++ ninjascript.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import re
 import pyperclip
+import matplotlib.pyplot as plt
+import easygui
 
-# import and save the SERIAL, SKU columns from DT_export.csv
-# the csv is the default format of the Export 2.0 tool
-data = pd.read_csv('DT_export.csv', usecols=['SERIAL', 'SKU'])
+# Select file dialog popup
+path = easygui.fileopenbox()
+
+# import and save the SERIAL, SKU columns from csv
+data = pd.read_csv(path, usecols=['SERIAL', 'SKU'])
 
 # this printout is a visual failsafe to compare clipboard contents
 serials_col = list(data['SERIAL'])
@@ -18,15 +22,34 @@ for i in data.SKU:
 
 # iterate through models list removing receiving format
 # and add total count
+counts_list = []
+models_list = []
+
 for elem in models_scan:
     model = re.sub(r"REC-", "", elem)
-    print(model, data.SKU.value_counts()[elem])
+    count = data.SKU.value_counts()[elem]
+    models_list.append(model)
+    counts_list.append(count)
+    print(model, count)
 
-# convert column contents to iterated string on new lines
+# Pandas datafram from the counts/models lists
+df = pd.DataFrame(columns=['MODELS', 'COUNT'])
+df['MODELS'] = models_list
+df['COUNT'] = counts_list
+
+# convert column contents to iterated string on new line
 # this will be proper format to paste into work website form
 clipboard = ''
 for s in data.SERIAL:
     clipboard = clipboard + s + '\n'
 
-#copy clipboard variable to OS (system agnostic) clipboard
+# copy clipboard variable to OS (system agnostic) clipboard
 pyperclip.copy(clipboard)
+
+# visuals
+plt.style.use('dark_background')
+df_sorted = df.sort_values(by='COUNT', ascending=False)
+plt.bar(df_sorted.MODELS, df_sorted.COUNT)
+plt.xticks(rotation=45, fontsize=7)
+plt.tight_layout()
+plt.show()
